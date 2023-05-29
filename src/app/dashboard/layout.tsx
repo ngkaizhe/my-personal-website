a0b5ae6c472d405d0b83be6070c34f969b5676c4
ngkaizhe <kaizhe1991@hotmail.com>
2023-05-29 22:14:08 +0800

    feat: add more link with style component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,26 +5,50 @@ import styled from "styled-components";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <DivWrapper>
-      <nav>
-        <ul>
-          <li>
-            <Link href="#">Nav1 link</Link>
-            <Link href="#">Nav2 link</Link>
-            <Link href="#">Nav3 link</Link>
-          </li>
-        </ul>
-      </nav>
+    <StyledDiv>
+      <StyledHeader>
+        <nav>
+          <ul>
+            <li>
+              <Link href="#">Nav1 link</Link>
+            </li>
+            <li>
+              <Link href="#">Nav2 link</Link>
+            </li>
+            <li>
+              <Link href="#">Nav3 link</Link>
+            </li>
+          </ul>
+        </nav>
+        <Link href="#">
+          <button>Contact</button>
+        </Link>
+      </StyledHeader>
       {children}
-    </DivWrapper>
+    </StyledDiv>
   );
 };
 
-const DivWrapper = styled.div`
+const StyledDiv = styled.div`
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+
+  li,
+  a,
+  button {
+    font-size: 16px;
+    color: #edf0f1;
+    text-decoration: none;
+  }
+`;
+
+const StyledHeader = styled.header`
   background-color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px 10%;
 `;
 
 export default DashboardLayout;
